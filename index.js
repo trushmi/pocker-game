ac99a0208298b0ffedb2efc66212c9af58cda37a
“Iryna <maria14trush@gmail.com>
2024-08-19 18:29:28 -0700

    add utility methods for adding players and generating random numbers

--- index.js
+++ index.js
@@ -32,5 +32,38 @@ const init = () => {
     .addEventListener("click", removePlayer);
   eventPlayAgainClicked();
 };
+// expose public methods
+// *-* utility methods *-*
+const addPlayer = () => {
+  const playersContainer = document.getElementById("players-container");
+  const handsContainers = document.getElementsByClassName("hand");
+  const section = document.createElement("section");
+  section.className = "hand";
+
+  const h1 = document.createElement("h1");
+  h1.className = "player-label";
+  h1.textContent = `Player ${handsContainers.length + 1}`;
+
+  const handCardWrapper = document.createElement("div");
+  handCardWrapper.className = "hand-card-wrapper";
+
+  section.appendChild(h1);
+  section.appendChild(handCardWrapper);
+  playersContainer.appendChild(section);
+};
+const getRandomNumberUpTo = (limit) => {
+  return Math.floor(Math.random() * limit);
+};
+
+const get90PercentChancePairIndex = (cards, deck) => {
+  const random = getRandomNumberUpTo(101);
+  if (random > 90) {
+    return getRandomNumberUpTo(deck.length);
+  }
+
+  const cardsSet = new Set(cards.map((cardObject) => cardObject.card));
+
+  return deck.findIndex((cardInDeck) => cardsSet.has(cardInDeck.card));
+};
 
 window.onload = Poker.init;
